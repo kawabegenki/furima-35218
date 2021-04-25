e7b0ebd6711574fcbc9fe0a4f6c1e72f6b96bea5
kawabegenki <kwbgnk.work@gmail.com>
2021-04-25 18:26:55 +0900

    調整コミット

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
     if user_signed_in?
       @item = Item.new
     else
-      # redirect_to user_session_path
+      redirect_to user_session_path
     end
   end
 
@@ -23,7 +23,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :price, :content, :category_id, :state_id, :charge_id, :from_id, :move_id,
-                                 :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :price, :content, :category_id, :state_id, :charge_id, :from_id, :move_id,:image).merge(user_id: current_user.id)
   end
 end
