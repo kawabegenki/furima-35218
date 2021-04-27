255bdbd1de951d89064c0da471c86c07a7ceeca2
kawabegenki <kwbgnk.work@gmail.com>
2021-04-27 20:42:03 +0900

    修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show,:update]
+  before_action :woop, only: [:edit,:update]
+  # git merge origin master
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -26,27 +28,28 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless @item.user == current_user
   end
 
   def update
-    if @item.user == current_user
-      if @item.update(item_params)
-        redirect_to item_path(@item.id)
-      else
-        render :edit
-      end
+    
+    if @item.update(item_params) 
+      redirect_to item_path(@item.id)
+    else
+      render :edit
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:title, :price, :content, :category_id, :state_id, :charge_id, :from_id, :move_id,
-                                 :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :price, :content, :category_id, :state_id, :charge_id, :from_id, :move_id,:image).merge(user_id: current_user.id)
   end
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def woop 
+    redirect_to action: :index unless @item.user == current_user 
   end
 end
